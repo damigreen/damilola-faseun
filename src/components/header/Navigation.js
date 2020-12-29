ef31389e92bacd168c974208beb05450121797a6
Damilola Faseun <fashfired@gmail.com>
2020-12-29 11:13:21 +0100

    Included navigation

---
+++ src/components/header/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Navigation() {
+
+  return (
+    <div id="navigation">
+      <div className="nav-item-wrap">
+        <a className="nav-item" href>Projects</a>
+        <a className="nav-item" href>Services</a>
+        <a className="nav-item" href>Process</a>
+        <a className="nav-item" href>About</a>
+        <a className="nav-item" href>Contact</a>
+      </div>
+    </div>
+  )
+}
+
+export default Navigation;
