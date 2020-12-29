9231107c845e9fb6ac6ca2aae0462a7191415dbe
Damilola Faseun <fashfired@gmail.com>
2020-12-29 11:13:06 +0100

    Added header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from './Navigation';
+import './Header.scss';
+
+function Header() {
+
+  return (
+    <div>
+      <p>Header</p>
+      <Navigation />
+    </div>
+  )
+}
+
+export default Header;
