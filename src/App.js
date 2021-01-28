c2dba67fb6aca5c8de8f650daae98d3120dac143
Damilola Faseun <damilola.faseun@gmail.com>
2021-01-28 23:42:11 +0100

    Added name and title to app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <header>
+        <h1>Damilola Faseun</h1>
+        <h1>Full Stack Developer</h1>
+
         <Header />
       </header>
     </div>
