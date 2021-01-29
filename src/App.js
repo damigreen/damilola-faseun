5724780899d14eeb81572752436473a06c4bc717
Damilola Faseun <damilola.faseun@gmail.com>
2021-01-29 21:51:14 +0100

    added navigation component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,18 @@ function App() {
       <header>
         <h1>Damilola Faseun</h1>
         <h1>Full Stack Developer</h1>
-
+        <ul>
+          <li>
+            home
+          </li>
+          <li>
+            portfolio
+          </li>
+          <li>
+            resume
+          </li>
+        </ul>
+        <p></p>
         <Header />
       </header>
     </div>
