145f7adeec0d650f1b03d8e16a5b58d414982c1e
Damilola Faseun <damilola.faseun@gmail.com>
2021-02-01 07:04:50 +0100

    Wired omponent to application

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import Header from "./components/header/"
 import './App.scss';
+import Portfolio from "./components/portfolio";
+import axios from 'axios';
+
+
 
 function App() {
+  const [ data, setData ] = useState({});
+  const dataUrl = './portfolioData.json'
+
+
+  useEffect(() => {
+    axios.get(dataUrl).then(data => {
+      setData(data);
+    });
+  }, [dataUrl] );
+
+  console.log(data);
   return (
     <div className="App">
       <header>
-        <h1>Damilola Faseun</h1>
-        <h1>Full Stack Developer</h1>
-
         <Header />
+        <Portfolio data={data.data.portfolio} />
       </header>
     </div>
   );
