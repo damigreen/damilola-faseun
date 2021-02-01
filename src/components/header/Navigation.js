5820286504cb9c64cd2a1f26702491ec9b22c4e6
Damilola Faseun <damilola.faseun@gmail.com>
2021-02-01 07:04:09 +0100

    Added navigation component

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,17 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const NavWrapper = styled.div`
+  /* background-color: brown; */
+  height: 90px;
+  padding: 5px;
+  border: 1px solid brown;
+`
+
+const NavBar = styled.div`
+  /* margin: 0 auto; */
+  /* background-color: yellow; */
+  height: 90px;
+  border: 1px solid brown;
+  width: 700px;
+  float: right;
+  
+  `
+
+const NavItem = styled.a`
+  border: 1px solid brown;
+  color: blue;
+  width: 45px;
+  padding-right: 10px;
+`
 
 function Navigation() {
 
   return (
-    <div id="navigation">
-      <div className="nav-item-wrap">
-        <a className="nav-item" href>Projects</a>
-        <a className="nav-item" href>Services</a>
-        <a className="nav-item" href>Process</a>
-        <a className="nav-item" href>About</a>
-        <a className="nav-item" href>Contact</a>
-      </div>
-    </div>
+    <NavWrapper>
+      <NavBar>
+        <NavItem>Work</NavItem>
+        <NavItem>Profile</NavItem>
+        <NavItem>Contact me</NavItem>
+      </NavBar>
+    </NavWrapper>
   )
 }
 
