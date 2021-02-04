1b1765f3c31cfd2173709eb1649a059f75ec287d
Damilola Faseun <damilola.faseun@gmail.com>
2021-02-04 22:40:42 +0100

    Created form style

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
+// const FormWrap = styled.div`
+const FormWrap = styled.form`
+    color: red;
+    display: inline-block;
+`
 
 const Contact = () => {
 
     return(
         <div>
             <h1>Contact me</h1>
-            <form>
-                <label for="Full Name">Full Name</label>
-                <input type="text" name="Full Name" />
-                <label for="Email">Email</label>
-                <input type="text" name="Email" />
-                <label for="Phone">Phone Number</label>
-                <input type="text" name="Phone" />
-                <label for="subject">Subject</label>
-                <input type="text" name="Full Name" />
-                <label for="message">Message</label>
-                <input type="text" name="Message" />
-            </form>
+            <FormWrap>
+                    <label for="Full Name">Full Name</label>
+                    <input type="text" name="Full Name" />
+
+                    <label for="Email">Email</label>
+                    <input type="text" name="Email" />
+
+                    <label for="Phone">Phone Number</label>
+                    <input type="text" name="Phone" />
+
+                    <label for="subject">Subject</label>
+                    <input type="text" name="Full Name" />
+
+                    <label for="message">Message</label>
+                    <input type="text" name="Message" />
+            </FormWrap>
         </div>
     )
 }
