22cb3366f49ea8a148f4e86e35e59032251d5e3f
Damilola <damilola.faseun@gmail.com>
2021-03-22 22:58:14 +0100

    Added a flex column wrap

--- src/AppStyled.js
+++ src/AppStyled.js
@@ -3,3 +3,10 @@ import styled, { css } from 'styled-components';
 export const borderStyle = `
     border: 2px solid red;
 `
+
+export const flexColumnWrap = `
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
