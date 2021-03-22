d185b040d1f418c25936c71b8121a24a1ec70185
Damilola <damilola.faseun@gmail.com>
2021-03-22 22:56:51 +0100

    modified services layout

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -11,37 +11,45 @@ const Services = () => {
     return (
         <ServicesWrapper>
             <ServicesRow>
-                <h1>Services</h1>
-
-                <div>
-                    <div>
-                        <h1>My Services Includes</h1>
+                <div className="services-box">
+                    <div className="services-intro">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry
                     </div>
                     <div>
-                        <div>
-                            <h1>Responsive Web Design</h1>
-                            <div>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        <h4 className="my-services-header">My Services Includes</h4>
+                    </div>
 
+                    <div className="services-row">
+                        <div className="services-data">
+                            <h4>Responsive Web Design</h4>
+                            <div className="services-content">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                             </div>
                         </div>
 
-                        <div>
-                            <h1>Search Engine Optimization</h1>
-                            <div>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                            </div>
-                        </div>
-
-                        <div>
-                            <h1>Mobile App Development</h1>
+                        <div className="services-data">
+                            <h4>Search Engine Optimization</h4>
                             <div>
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                             </div>
                         </div>
 
-                        <div>
-                            <h1>Database Management</h1>
+                        <div className="services-data">
+                            <h4>Mobile App Development</h4>
+                            <div>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </div>
+                        </div>
+
+                        <div className="services-data">
+                            <h4>Database Management</h4>
+                            <div>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </div>
+                        </div>
+
+                        <div className="services-data">
+                            <h4>Wordpress Theme Development</h4>
                             <div>
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                             </div>
--- src/components/services/ServicesStyled.js
+++ src/components/services/ServicesStyled.js
@@ -1,13 +1,62 @@
 import styled, { css } from 'styled-components'
 import {
-    borderStyle
+    borderStyle,
+    flexColumnWrap,
 } from '../../AppStyled';
+
+
+
+const servicesRow = `
+`
 
 export const  ServicesWrapper = styled.div`
     /* margin-top: 300px; */
-    border: 2px solid green;
+    box-sizing: border-box;
 ` 
 
 export const ServicesRow = styled.div`
-    ${borderStyle};
+    ${servicesRow};
+
+
+    .services-box {
+        text-align: center;
+    }
+
+    .services-intro {
+        padding: 20px 30px;
+        font-size: 1.901rem;
+    }
+
+    .my-services-header {
+        font-size: 1.14561rem;
+        padding-top: 20px;
+        padding-bottom: 20px;
+        border-top: 1px solid rgba(49,49,49,0.1);
+        border-bottom: 1px solid rgba(49,49,49,0.1);
+        text-transform: uppercase;
+        letter-spacing: 0.120rem;
+    }
+
+    .services-row {
+        /* margin: 20px; */
+    }
+
+    .services-data {
+        border: 1px solid red;
+        ${flexColumnWrap};
+        padding: 40px;
+        border-bottom: 1px solid rgba(49,49,49,0.1);
+
+        h4 {
+            /* font-size: 1.03rem; */
+            text-transform: uppercase;
+            letter-spacing: 0.120rem;
+            background: red;
+        }
+
+        div {
+            line-height: 22px;
+            margin: 20px auto;
+        }
+    }
 `
