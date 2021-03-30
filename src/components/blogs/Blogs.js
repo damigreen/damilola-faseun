0e24d9498dfa1fac515c8f583011c28391e695ef
Damilola <damilola.faseun@gmail.com>
2021-03-30 23:01:56 +0100

    Created a blog component

---
+++ src/components/blogs/Blogs.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Blogs = () => {
+    
+    return (
+        <div>
+            <h1>Blogs</h1>
+            <h4>Welcome to React Native</h4>
+            <p>React Native...</p>
+        </div>
+    )
+}
+
+export default Blogs
