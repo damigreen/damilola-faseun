d7001a5aa5ba8801da357e2d83b24498da4f453e
Damilola Faseun <damilola.faseun@gmail.com>
2021-04-30 22:16:01 +0100

    Added layout for blog

---
+++ src/components/blogs/BlogStyled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const BlogWrapper = styled.div`
+  padding: 0;
+  margin: 0 auto;
+  width: 100%;
+  height: auto;
+`
+
+export const BlogRow = styled.div`
+  background-color: blue;
+  color: #333;
+`
