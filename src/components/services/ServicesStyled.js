3ed1fea5de3a8a28aeaff9cb8fd8b113adcdbc77
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-17 21:58:48 +0100

    modified service layout

--- src/components/services/ServicesStyled.js
+++ src/components/services/ServicesStyled.js
@@ -16,10 +16,22 @@ export const  ServicesWrapper = styled.div`
 
 export const ServicesRow = styled.div`
     ${servicesRow};
-
+    width: 100%;
+    margin: 0;
+    padding: 0;
 
     .services-box {
+        display: flex;
+        flex-flow: column wrap;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
         text-align: center;
+        /* border: 1px solid green; */
+
+        /* @media only screen and (min-width : 480px) and (max-width : 595px) {
+            flex: 0 0 50%;
+        }  */
     }
 
     .services-intro {
@@ -37,21 +49,42 @@ export const ServicesRow = styled.div`
         letter-spacing: 0.120rem;
     }
 
-    .services-row {
-        /* margin: 20px; */
+    .services-items {
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: center;
+        width: 100%;
+        max-width: 90rem;
+        border: 1px solid red;
+
+
     }
 
     .services-data {
-        border: 1px solid red;
         ${flexColumnWrap};
+        /* margin: 0.9rem 1.2rem;
+        border: 1px solid gray; */
+
+        /* @media only screen and (min-width : 480px) and (max-width : 595px) {
+            flex: 0 0 33%;
+        }  */
+
         padding: 40px;
         border-bottom: 1px solid rgba(49,49,49,0.1);
+        border-right: 1px solid rgba(49,49,49,0.1);
+
+        .service-img {
+            max-width: 280px;
+            width: 100%;
+        }
 
         h4 {
-            /* font-size: 1.03rem; */
+            font-size: 1.03rem;
             text-transform: uppercase;
             letter-spacing: 0.120rem;
-            background: red;
+            /* background: red; */
+            text-align: left;
+            padding: 0.4rem 0.7rem;
         }
 
         div {
