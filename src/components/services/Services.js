b478a0523b0d8ef63902745df47cadb08b1683ba
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-17 21:59:12 +0100

    Added image setion to service

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -13,45 +13,58 @@ const Services = () => {
             <ServicesRow>
                 <div className="services-box">
                     <div className="services-intro">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry
                     </div>
                     <div>
-                        <h4 className="my-services-header">My Services Includes</h4>
+                        <h4 className="my-services-header">Services</h4>
                     </div>
 
-                    <div className="services-row">
+                    <div className="services-items">
                         <div className="services-data">
+                            <img className="service-img" src />
                             <h4>Responsive Web Design</h4>
                             <div className="services-content">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                             </div>
                         </div>
 
                         <div className="services-data">
+                            <img className="service-img" src />
                             <h4>Search Engine Optimization</h4>
                             <div>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                             </div>
                         </div>
 
                         <div className="services-data">
+                            <img className="service-img" src />
                             <h4>Mobile App Development</h4>
                             <div>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                             </div>
                         </div>
 
                         <div className="services-data">
+                            <img className="service-img" src />
                             <h4>Database Management</h4>
                             <div>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                             </div>
                         </div>
 
                         <div className="services-data">
+                            <img className="service-img" src />
                             <h4>Wordpress Theme Development</h4>
                             <div>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            </div>
+                        </div>
+
+                        <div className="services-data">
+                            <img className="service-img" src />
+                            <h4>Wordpress Theme Development</h4>
+                            <div>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                             </div>
                         </div>
                     </div>
