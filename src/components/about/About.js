c6940ca376f2fc7fb180a66d3a257c17244ea448
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-18 17:04:34 +0100

    created initial layout for about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {
-  AboutWrap
+  AboutWrap,
+  AboutContent,
+  AboutImage,
 } from './AboutStyled';
 
 
@@ -8,7 +10,22 @@ const About = () => {
 
   return (
     <AboutWrap>
-      <h1>About me</h1>
+      <AboutImage>
+        <img className="about-image" alt="Image for the about section" />
+        <div className="frame-one"></div>
+        <div className="frame-two"></div>
+      </AboutImage>
+      <AboutContent>
+        <h1>About me</h1>
+        <p>
+          I realize the dominating thoughts of my mind will eventually
+          reproduce themselves in outward, physical action, and gradually transform
+          themselves into physical reality, therefore, I will concentrate my thoughts for
+          thirty minutes daily, upon the task of thinking of the person I intend to
+          become, thereby creating in my mind a clear mental picture of that person.
+        </p>
+      </AboutContent>
+      
     </AboutWrap>
   )
 }
--- src/components/about/AboutStyled.js
+++ src/components/about/AboutStyled.js
@@ -1,8 +1,20 @@
 import styled from 'styled-components';
 import {
-  borderStyle
+  borderStyle,
+  flexRowWrap,
 } from '../../AppStyled';
 
 export const AboutWrap = styled.div`
   ${borderStyle}
+  ${flexRowWrap};
+`
+
+export const AboutImage = styled.div`
+  ${borderStyle}
+  
+`
+
+export const AboutContent = styled.div`
+  ${borderStyle}
+
 `
