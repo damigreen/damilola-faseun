39a33923b1b45eec6038da0fc290ac54c6c45552
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-18 12:21:59 +0100

    appliation information

--- src/App.js
+++ src/App.js
@@ -6,7 +6,69 @@ import Portfolio from "./components/portfolio/";
 import Contact from './components/contact/';
 import Services from './components/services/';
 
+/*
 
+* Application Information
+
+Header
+	Navigation
+
+		fonts-family:
+		font-size: 
+		color
+		color-active
+		color-hover
+		color-visited
+
+	HeroTextPrimary 
+    font-family:
+		font-size:
+		font-weight:
+    color:
+    background:
+  
+  HeroTextSecondary 
+  font-family:
+  font-size:
+  font-weight:
+  color:
+  background:
+
+    Button1
+      color:
+      background:
+    Button2
+      color:
+      background:
+
+Body
+  header-primary
+    font-family
+    font-weight:
+    color: 
+    background:
+
+  header-secondary
+    font-family
+    font-weight:
+    color: 
+    background:
+  
+  paragraph
+    font-family
+    font-weight:
+    color: 
+    background:
+  
+Buttons 
+  color:
+  background:
+  hover
+  visited
+  link
+  focus:  
+
+*/
 
 
 function App() {
