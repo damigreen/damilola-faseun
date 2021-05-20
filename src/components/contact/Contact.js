3aa9a5da74c2acac28b6ffade9e6a0c06e98cb06
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-20 16:51:39 +0100

    Created layout for contact

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-import {
-    ContactWrapper,
-} from './ContactStyled';
 import useForm from 'hooks/formHook';
+import Testimonial from './Testimonial';
+import {
+    ContactWrap,
+    TestimonialSection,
+    FormWrap,
+    FieldsWrap,
+} from './ContactStyled';
 
-// const FormWrap = styled.div`
-const FormWrap = styled.form`
-    color: red;
-    display: inline-block;
-`
+
 
 const Contact = () => {
 
@@ -24,39 +23,48 @@ const Contact = () => {
 
 
     return(
-      <ContactWrapper>
-        <div className="contact-wrapper">
-          <div className="form-wrapper">
-            <h3>Get in touch</h3>
-            <div>
-              <FormWrap>
-                <form>
-                  <div>
-                    <label for="fullname">Full name <span>*</span></label>
-                    <input {...fullName.form} className="input-field fn" />
-                  </div>
-                  <div>
-                    <label for="email">Email <span>*</span></label>
-                    <input {...email.form} className="input-field " />
-                  </div>
-                  <div>
-                    <label for="subject">Subject <span>*</span></label>
-                    <input {...subject.form} className="input-field sub" />
-                  </div>
-                  <div>
-                    <label for="company">Company </label>
-                    <input {...company.form} className="input-field" />
-                  </div>
-                  <div>
-                    <label for="message">Message <span>*</span></label>
-                    <textarea {...message.form} className="input-field fn" />
-                  </div>
-                </form>
-              </FormWrap>
+      <ContactWrap>
+        <div className="contact-wrap">
+          <FieldsWrap>
+            <div className="fields-wrap">
+                <h3>Get in touch</h3>
+
+                <FormWrap>
+                  <form>
+                    <div>
+                      <label for="fullname">Full name <span>*</span></label>
+                      <input {...fullName.form} className="input-field fn" />
+                    </div>
+                    <div>
+                      <label for="email">Email <span>*</span></label>
+                      <input {...email.form} className="input-field " />
+                    </div>
+                    <div>
+                      <label for="subject">Subject <span>*</span></label>
+                      <input {...subject.form} className="input-field sub" />
+                    </div>
+                    <div>
+                      <label for="company">Company </label>
+                      <input {...company.form} className="input-field" />
+                    </div>
+                    <div>
+                      <label for="message">Message <span>*</span></label>
+                      <textarea {...message.form} className="input-field fn" />
+                    </div>
+                  </form>
+                </FormWrap>
+              
             </div>
-          </div>
+          </FieldsWrap>
+
+          <TestimonialSection>
+            <div className="testimonial-section">
+              <Testimonial />
+            </div>
+          </TestimonialSection>
+
         </div>
-      </ContactWrapper>
+      </ContactWrap>
     )
 }
 
--- src/components/contact/ContactStyled.js
+++ src/components/contact/ContactStyled.js
@@ -1,10 +1,33 @@
 import styled from 'styled-components';
 
 
-export const ContactWrapper = styled.div`
-    border: 1px solid red;
+export const ContactWrap = styled.div`
+
+    .contact-wrap {
+        border: 1px solid gold;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        /* flex: 0 0 40%; */
+    }
+`
+
+export const FieldsWrap = styled.div`
+
+    .fields-wrap {
+        border: 1px solid green;
+    }
 `
 
 export const FormWrap = styled.div`
-    background-color: red;
+    border: 1px solid red;
+`
+
+export const TestimonialSection = styled.div`
+
+    .testimonial-section {
+        border :1px solid  purple;
+        /* flex: 0 0 30%; */
+
+    }
 `
