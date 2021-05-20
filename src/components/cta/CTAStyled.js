3c5dcb5fbaedc3ca0450cf54270ba20ebfe28457
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-20 13:31:58 +0100

    CTA page layoout style

--- src/components/cta/CTAStyled.js
+++ src/components/cta/CTAStyled.js
@@ -2,5 +2,97 @@ import styled from 'styled-components';
 
 
 export const CTAWrap = styled.div`
-  border: 1px solid red;
+  position: relative;
+  top: 0;
+  left: 0;
+
+  .cta-wrapper {
+    background: url('../../../images/cta-background.png');
+    background-position: 50% 50%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+
+  }
+
+  .parallax {
+    position: relative;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 0;
+  }
+
+  .cta-wrap {
+    height: auto;
+    min-height: 300px;
+    max-height: 400px;
+    margin:0 auto;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    /* justify-content: space-between; */
+    justify-content: center;
+    align-items: center;
+    max-width: 80rem;
+    padding: 0.34rem 1.989rem;
+
+    @media only screen and (max-width: 767px) {
+      padding: 3.1rem;
+      text-align: center;
+      flex-flow: column wrap;
+      justify-content: center;
+    }
+
+    @media only screen and (min-width: 1200px) {
+      min-height: 300px;
+    }
+
+  }
+
+  .cta-text {
+    flex: 0 0 60%;
+    color: #FFF;
+    color: #9B7874;
+    color: #f7f0f0;
+
+    font-size: 1.0393456rem;
+    font-size: 1.7393456rem;
+    font-weight: 500;
+    letter-spacing: 0.1245rem;
+
+    @media only screen and (max-width: 767px) {
+      font-size: 1.39983rem;
+      margin-bottom: 3rem;
+    }
+  }
+
+  .button-wrap {
+    flex: 0 0 30%;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+
+    button {
+      padding: 0.4961rem 1rem;
+      height: auto;
+      width: 9rem;
+      color: #FFF;
+      text-transform: uppercase;
+      font-size: 0.88982rem;
+      border-radius: 3px;
+      
+      :first-child {
+        margin: 0 1.7489rem 0.21rem 0;
+        background-color: #862020;
+        border: 1px solid #862020;
+      }
+
+      :last-child {
+        background-color: #6FA568;
+        border: 1px solid #6FA568;
+      }
+    }
+  }
 `
