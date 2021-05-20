2976ae5ed495b16da103b9ffb8f91b9a38fb636a
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-20 13:31:09 +0100

    Added text to cta page

--- src/components/cta/CTA.js
+++ src/components/cta/CTA.js
@@ -9,15 +9,16 @@ const CTA = () => {
 
   return (
     <CTAWrap>
-      <div className="cta-wrap">
-        <div>
-          <p>
-          is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.
-          </p>
-          <div>
-            <button>Hire me</button>
-            <button>Get in touch</button>
-          </div>
+      <div className="cta-wrapper">
+        <div className="cta-wrap parallax">
+            <p className="cta-text">
+              is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.
+            </p>
+
+            <div className="button-wrap">
+              <button>Hire me</button>
+              <button>Get in touch</button>
+            </div>
         </div>
       </div>
     </CTAWrap>
