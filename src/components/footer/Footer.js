955d2a6bd81fe9dc092dbff754f3744e3ac8919b
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-24 16:26:47 +0100

    improved footer layout

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,60 @@
 import React from 'react';
+import useForm from 'hooks/formHook';
+import {
+    FooterWrap,
+    InfoWrap,
+    SiteMapWrap,
+    SubscriptionWrap,
+
+} from './FooterStyled';
+
 
 const Footer = () => {
+    const email = useForm('email');
 
     return (
-        <div>
-            <h1>Footer</h1>
-            <div>Copyrights @2021 Damilola Faseun</div>
-        </div>
+        <FooterWrap>
+            <div className="footer-wrap">
+                <div className="footer-row">
+                    <InfoWrap>
+                        <div className="info-wrap">
+                            <img src={`../../../icons/footer-logo-icon.png`} alt="company logo`" />
+                            <h2>Damigreentech</h2>
+                        </div>
+                        <div className="social-icons">
+                            <div>Github</div>
+                            <div>LinkedIn</div>
+                            <div>Twitter</div>
+                            <div>Behance</div>
+                        </div>
+                        {/* <h2>Greenmtech</h2> */}
+                    </InfoWrap>
+                    <SiteMapWrap>
+                        <h3>Site Map</h3>
+                        <ul className="sitemap-wrap">
+                            <li>About Me</li>
+                            <li>Services</li>
+                            <li>Contact</li>
+                        </ul>
+                    </SiteMapWrap>
+                    <SubscriptionWrap>
+                        <h3>Subscribe</h3>
+                        <p>Enter your mail to get notified about new solutions and offers</p>
+                        <div>
+                            <form>
+                                <label for="subscription">
+                                    <input {...email.form } placeholder="Email" />
+                                </label>
+                            </form>
+                        </div>
+                    </SubscriptionWrap>
+                </div>
+
+                <div className="copyrights">
+                    (c) 2021 Greenmtech Inc. All rights reserved
+                </div>
+            </div>
+        </FooterWrap>
     )
 }
 
