67f8be8be42338ac4ff9ec69f564d17c49ed671a
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-26 21:31:05 +0100

    Created a responsive hero image

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,28 +10,31 @@ import {
   ButtonDiv,
   HeroImage,
 } from './HeaderStyle'
-import { HeroHeading } from 'AppStyled';
+import {
+  HeroHeading,
+  HeroHeadingName,
+  HeroDescription,
+} from 'AppStyled';
 
 
 function Header() {
+  {/* <HeroWrap>
+    <HeroImage>
+      <Navigation />
+        <HeroText>
+          <HeroContent>
+            <NameDiv>Damilola Faseun</NameDiv>
+            <TitleDiv>Freelance Full stack Developer based in Lagos, Nigeria</TitleDiv>
+            <ButtonDiv>
+              <AboutButton>About Me</AboutButton>
+            </ButtonDiv>
+          </HeroContent>
+        </HeroText>
+    </HeroImage>
+  </HeroWrap> */}
 
   return (
     <HeaderWrap>
-      {/* <HeroWrap>
-        <HeroImage>
-          <Navigation />
-            <HeroText>
-              <HeroContent>
-                <NameDiv>Damilola Faseun</NameDiv>
-                <TitleDiv>Freelance Full stack Developer based in Lagos, Nigeria</TitleDiv>
-                <ButtonDiv>
-                  <AboutButton>About Me</AboutButton>
-                </ButtonDiv>
-              </HeroContent>
-            </HeroText>
-        </HeroImage>
-      </HeroWrap> */}
-
       <div className="header-wrap">
         <HeroWrap>
           <HeroImage>
@@ -41,13 +44,18 @@ function Header() {
         <Navigation />
 
         <HeroText>
-            <HeroHeading>Damilola Faseun</HeroHeading>
-            <TitleDiv>Freelance Full stack Developer based in Lagos, Nigeria</TitleDiv>
+            <HeroHeadingName>Hi, I'm Damilola Faseun</HeroHeadingName>
+            <HeroHeading>Full stack Developer</HeroHeading>
+            <HeroHeading> based in Lagos, Nigeria</HeroHeading>
+            <HeroDescription>Engineering top-notch <span className="slide-web">custom web development</span>
+                                                    <span className="slide-mobile" style={{ display: 'none' }}>mobile apps</span>
+                                                    <span className="slide-software" style={{ display: 'none' }}>software product</span>
+                                                    <span className="slide-ecommerce" style={{ display: 'none' }}>e-commerce solutions</span> with a mix of creative ideas and technology with digital knowledge to </HeroDescription>
+            <TitleDiv></TitleDiv>
             <ButtonDiv>
               <AboutButton>About Me</AboutButton>
             </ButtonDiv>
         </HeroText>
-
       </div>
     </HeaderWrap>
   )
--- src/components/header/HeaderStyle.js
+++ src/components/header/HeaderStyle.js
@@ -4,53 +4,50 @@ import styled, { css, ThemeProvider } from 'styled-components';
 
 export const HeaderWrap = styled.div`
   color: #EEEEEF;
-  /* height: 800px; */
-
-
-
+  width: 100%;
 
   .header-wrap {
     height: 100vh;
-    width: 100%;
-    position: relative;
-    top: 0;
-    left: 0;
-    border: 1px solid red;
+    width: auto;
     
     @media only screen and (max-width: 1200px) {
+      position: relative;
+      width: 100vw;
+      left: 0;
+      right: 0;
       height: 493px;
     }
   }
 `
 
 export const HeroWrap = styled.div`
-  /* background-color: yellow; */
-  width: 100%;
   height: auto;
   height: 100%;
-
   background: conic-gradient(from 163.17deg at 50% 50%, rgba(72, 73, 86, 0.73) 0deg, rgba(47, 79, 79, 0.502145) 180.62deg, rgba(94, 215, 215, 0.456489) 232.5deg, rgba(47, 79, 79, 0.258928) 277.18deg, rgba(59, 67, 73, 0.158272) 309.38deg, rgba(180, 46, 110, 0) 360deg), #191A1D;
   background-blend-mode: soft-light, normal;
   border: 1px solid rgba(0, 0, 0, 0.3);
-  border: 4px solid brown;
   box-sizing: border-box;
+  /* border: 4px solid brown; */
 `
 
 export const HeroImage = styled.div`
-  border: 5px solid green;
+  position: absolute;
+  left: 0;
+  bottom: 0;
   width: 100%;
   height: 100%;
-  background: url('../../../images/hero-image.jpg');
+  background: url('../../../images/hero-image.png');
   background-position: center top;
   background-size: cover;
   background-repeat: no-repeat;
   transform: translate(0px, 0px);
   visibility: inherit;
   opacity: 0.2;
+  /* border: 5px solid green; */
 
   @media only screen and (max-width: 767px) {
-    /* background-size: contain; */
-    height: 500px;
+    width: 100%;
+    height: 493px;
   }
 `
 
@@ -65,14 +62,29 @@ export const HeroText = styled.div`
   align-items:flex-start;
   margin: 0 auto;
   margin-top: 80px;
-  width: 100%;
-  max-width: 1800px;
+  width: auto;
+  /* width: 100%; */
+  /* max-width: 1800px; */
   height: auto;
   border: 1px solid gold;
   color: #fff;
   transition: color .3s ease,opacity .3s ease .65s;
   z-index: 7;
   padding: 0 7rem;
+  padding-right: 0;
+
+  @media only screen and (max-width: 767px) {
+    margin-left: auto;
+    margin-right: auto;
+    padding: 0;
+    width: auto;
+    max-width: 100%;
+    align-items: center;
+    justify-content: center;
+    padding: 0 3rem;
+    /* padding-left: 7rem; */
+    /* vertical-align: center; */
+  }
 
 `
 
