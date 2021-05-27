1019ae6bae86af448429a68b92a6725d0392d9f6
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-27 21:47:17 +0100

    Added a test component

---
+++ src/components/test/Test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// import './Test.scss';
+
+
+const Test = () => {
+
+  return (
+    <div class="container">
+      <div class="button">Center  out</div>
+      <div class="button">Left  Right  Left</div>
+      <div class="button">Left  Right  Right</div>
+      <div class="button">Top  Bottom  Top</div>
+      <div class="button">Skew Fill Left  Right</div>
+      
+      <div class="button">Rounded Corners</div>
+      <div class="button">Scale</div>
+      <div class="button">Border (Inner Shadow)</div>
+      <div class="button">Border (Outer Shadow)</div>
+    </div>
+  )
+}
+
+export default Test;
