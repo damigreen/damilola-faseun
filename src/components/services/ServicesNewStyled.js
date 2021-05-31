29786d04ad381a4c4564b194167a1c52ec73d94d
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-31 22:12:13 +0100

    Added animation to services icon

--- src/components/services/ServicesNewStyled.js
+++ src/components/services/ServicesNewStyled.js
@@ -9,6 +9,7 @@ import {
 export const ServicesWrap = styled.div`
   margin: 0;
   padding: 0;
+  background-color: #f8f8f8;
   
   .services-wrap {
     width: auto;
@@ -38,22 +39,26 @@ export const ServicesRow = styled.div`
   `
 
 export const ServiceInfo = styled.div`
+  flex: 0 0 30.33%;
 
   .service-info {
-    border: 1px solid green;
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     align-items: flex-start;
-
+    border: 1px solid green;
+    
     @media only screen and (min-width: 1024px) {
-      align-items: flex-start;
+      padding-top: 50px;
+      /* align-items: flex-start; */
+      /* align-items: center; */
+      /* justify-content: center; */
     }
   }
   
   .col-title {
     ${HeadingTertiary};
-    align-self: center;
+    align-self: left;
 
     @media only screen and (min-width: 1024px) {
       ${HeadingSecondary}
@@ -66,13 +71,14 @@ export const ServiceInfo = styled.div`
     align-self: center;
     margin-top: 1rem;
     width: 100%;
-    text-align: center;
+    text-align: left;
 
     @media only screen and (min-width: 1024px) {
       ${HeadingPrimary};
       margin-left: 0;
       display: flex;
       flex-direction: column;
+      text-align: left;
     }
   }
 
@@ -81,34 +87,104 @@ export const ServiceInfo = styled.div`
     display: none;
 
     @media only screen and (min-width: 1024px) {
-      
+
     }
   }
 `
 
 export const ServiceCard = styled.div`
   width: auto;
+  flex: 0 0 70.33%;
 
   .service-card {
-    border: 1px solid purple;
+    /* border: 1px solid purple; */
     display: flex;
     flex-direction: column;
+    border: 2px solid green;
+    width: 100%;
+    padding: 50px 0;
 
     @media only screen and (min-width: 1024px) {      
-
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      justify-content: space-between;
     }
-
+    
     .col-card {
-      border: 1px solid yellow;
-      /* padding: 0 3rem; */
-    }
+      border: 1px solid brown;
+      padding: 0 3rem;
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      
+      div {
+        margin-top: 50px;
+      }
 
+      @media only screen and (min-width: 1024px) {
+        padding: 0 20px;
+        flex: 0 0 42.9987673%;
+
+        img {
+          width: 100%;
+          max-width: 55px;
+          height: 55px;
+          margin-top: 50px;
+        }
+  
+      }
+
+      @media only screen and (min-width: 1300px) {
+        padding: 0 20px;
+        flex: 0 0 44.87673%;
+
+
+      }
+    }
+    
     .col-img {
-
+      /* width: 55px;
+      height: 55px; */
+      width: 100%;
+      max-width: 55px;
+      height: auto;
+      margin-top: 50px;
+      padding: 0 20px;
+      @media only screen and (min-width: 1024px) {          
+      }
     }
 
+    .bounce {
+      position: relative;
+      /* left:50%; */
+      bottom:0;
+      /* margin-top:-25px;
+      margin-left:-25px; */
+      /* height:50px;
+      width:50px; */
+      /* background:red; */
+      animation: bounce 2s infinite;
+      -webkit-animation:bounce 3s infinite ease-in-out;
+    }
+
+
+    @keyframes bounce {
+      0% {
+        transform: translateY(-5px);
+      }
+      50% {
+        transform: translateY(10px);
+      }
+      100% {
+        transform: translateY(-5px);
+      }
+    }
+    
     .col-inner {
-      /* padding: 0 2rem; */
+      
+      @media only screen and (min-width: 1024px) {      
+      }
     }
   }
 `
