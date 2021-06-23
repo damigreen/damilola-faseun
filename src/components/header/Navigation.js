4be1f772a6d6c17a5ca9c426fdd0b8e5b168a320
Damilola Faseun <damilola.faseun@gmail.com~>
2021-06-23 12:25:37 +0100

    Change navigation route on links

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -20,10 +20,10 @@ function Navigation() {
 
           <div className="nav-bar">
             <ul className="nav-list">
-              <li><a className="nav-link" href="#about">About</a></li>
-              <li><a className="nav-link" href="#services">Services</a></li>
-              <li><a className="nav-link" href="#portfolio">Work</a></li>
-              <li><a className="nav-link insight" href="#portfolio">Insights</a></li>
+              <li><a className="nav-link" href="/#about">About</a></li>
+              <li><a className="nav-link" href="/#services">Services</a></li>
+              <li><a className="nav-link" href="/#portfolio">Work</a></li>
+              <li><a className="nav-link insight" href="/#portfolio">Insights</a></li>
             </ul>
           </div>
 
