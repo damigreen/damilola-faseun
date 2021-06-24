9a3c1e52472af19005224e54838d14f7c0d0d10a
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-24 17:40:26 +0100

    Changed about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -21,11 +21,11 @@ const About = () => {
         <AboutContent>
           <h2>About me</h2>
           <p>
-            I realize the dominating thoughts of my mind will eventually
-            reproduce themselves in outward, physical action, and gradually transform
-            themselves into physical reality, therefore, I will concentrate my thoughts for
-            thirty minutes daily, upon the task of thinking of the person I intend to
-            become, thereby creating in my mind a clear mental picture of that person.
+          I am a <b>creative front-end developer</b>. I use JavaScript and React to create highly interactive and user friendly web experience and products.
+          <b>I create web, Android, IOS and cross platform applications</b> with focus on  meeting brands needs in terms of functionality and apperance.
+          <br />My passion is creating <b>web technologies and innovations</b> that helps you reach more customers and <b>aids business growth</b> using the latest and in demand technologies.
+
+          As a highly diligent and hardworking freelancer, I have <b>designed, developed, and deployed</b> several top quality work that cuts accross different sectors like small business, finance, education etc. <b>providing solutions that meets business needs and guranteed</b> to increase conversion rate of your products.
           </p>
         </AboutContent>
       </div>
