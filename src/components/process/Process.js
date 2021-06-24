be144bfa8deaaa03cecf21e90656a5b84d3fea32
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-24 20:32:07 +0100

    Added process content

--- src/components/process/Process.js
+++ src/components/process/Process.js
@@ -23,7 +23,7 @@ const Process = () => {
                 <div className="card-bg design flex-md end">
                   <h4>Design</h4>
                 </div>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
+                <p>I create wireframes, and sketch designs to represent the idea I want to implement.</p>
               </div>
             </ProcessCard>
 
@@ -32,7 +32,7 @@ const Process = () => {
                 <div className="card-bg develop flex-md end">
                   <h4>Develop</h4>
                 </div>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>                
+                <p>After crafting the right design prototype for the product, I proceed to developing the product</p>                
               </div>
             </ProcessCard>
 
@@ -41,7 +41,7 @@ const Process = () => {
                 <div className="card-bg deploy flex-md end">
                   <h4>Deploy</h4>
                 </div>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
+                <p>Following implementation, I host the website over a secured server and deploy apps to google and apple stores</p>
               </div>
             </ProcessCard>
           </div>
