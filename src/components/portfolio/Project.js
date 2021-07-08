69822aa4737785e9985e329a1900e2ee136e439b
Damilola Faseun <damilola.faseun@gmail.com>
2021-07-08 16:13:19 +0100

    Project component init

---
+++ src/components/portfolio/Project.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { 
+  ProjectWrap
+} from './ProjectStyled'
+
+const Project = () => {
+
+
+  return (
+    <ProjectWrap>
+      <div>
+        <h1>Projects</h1>
+      </div>
+    </ProjectWrap>
+  )
+}
+
+export default Project;
+;
