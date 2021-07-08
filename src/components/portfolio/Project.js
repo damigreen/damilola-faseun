8eae6bae9399c491a5d4a47f0d0b2a705a80274e
Damilola Faseun <damilola.faseun@gmail.com>
2021-07-08 21:56:49 +0100

    add new project component

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -10,6 +10,37 @@ const Project = () => {
     <ProjectWrap>
       <div>
         <h1>Projects</h1>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <button>View Website</button>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
+        <div>I am going to be successful</div>
       </div>
     </ProjectWrap>
   )
--- src/components/portfolio/ProjectStyled.js
+++ src/components/portfolio/ProjectStyled.js
@@ -3,4 +3,3 @@ import styled from 'styled-components';
 export const ProjectWrap = styled.div`
   border: 1px solid gold;
 `
-
