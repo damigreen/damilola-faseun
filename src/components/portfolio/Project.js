aa79f324d6982f18d4b2eda632a6e3556257c481
Damilola Faseun <damilola.faseun@gmail.com>
2021-07-12 20:49:29 +0100

    built individual project section

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -1,17 +1,93 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect } from 'react';
+import Navigation from '../header/Navigation';
+
 import { 
-  ProjectWrap
-} from './ProjectStyled'
+  ProjectWrap,
+  ProjectHero,
+  ProjectSection,
+  SectionC,
+} from './ProjectStyled';
 
+// const sectionOption =  {
+//   rootMargin: "0",
+// }
 
-const Project = ({ project }) => {
-  console.log(project) 
+const handleIntersection = () => {
+  
+  try {
+    const section = document.querySelector('.section-c');
+    const intro = document.querySelector('.intro-b');
+
+    const sectionObserver = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        console.log(entry)
+    
+        if (!entry.isIntersecting) {
+          intro.classList.add('absolute');
+          intro.classList.remove('fixed');
+          console.log('Not intersect---------------------------------')
+        } else {
+          intro.classList.add('fixed');
+          intro.classList.remove('absolute');
+          console.log('intersect---------------------------------')
+        }
+      })
+    }, );
+  
+    sectionObserver.observe(intro);
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const Project = ({ currentProject }) => {
+  const projectCover = '../../../images/sample-hero.jpg';
+  const projectSkeleton = '../../../images/project-skeleton.png';
+
+  useEffect(() => {
+    handleIntersection();
+  }, []);
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  });
 
 
   return (
     <ProjectWrap>
       <div className='project-wrap'>
-        <h1 className='primary-heading flex-center'>Projects</h1>
+      <Navigation />
+      <ProjectHero>
+        <div className='project-hero'>
+            {/* < */}
+        </div>
+      </ProjectHero>
+      <ProjectSection>
+        <div className='project-section section-margin flex-col'>
+          <section className='section-a section-padding flex-col'>
+            <h2>The Breif</h2>
+            <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+            </p>
+          </section>
+          <section className="section-b section-padding flex-center relative">
+            <img alt='Project cover image' src={projectCover}/>
+          </section>
+          {/* <section className="wrap-flex section-padding section-c"> */}
+          <SectionC className="section-padding section-c relative">
+            <div className="intro-b section-padding absolute">
+              <h3>Blogs Keep</h3>
+              <p project-description>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+            </div>
+            <figure className='browser float-r'>
+              <img alt='Project image landing page' src={projectSkeleton} />
+            </figure>
+          </SectionC>
+        </div>
+        <div>
+
+        </div>
+      </ProjectSection>
       </div>
     </ProjectWrap>
   )
--- src/components/portfolio/ProjectStyled.js
+++ src/components/portfolio/ProjectStyled.js
@@ -1,15 +1,125 @@
 import styled from 'styled-components';
-import { HeadingPrimary } from 'AppStyled';
+import { HeadingPrimary, HeadingSecondary } from 'AppStyled';
 
 
 export const ProjectWrap = styled.div`
-  border: 1px solid gold;
 
   .project-wrap {
-    border: 1px solid green;
   }
 
   .primary-heading {
     ${HeadingPrimary};
+  }
+`
+
+export const ProjectHero = styled.div`
+
+  .project-hero {
+    /* width: 500px; */
+    max-width: 100%;
+    height: 500px;
+    border: 1px solid red;
+    background: url('../../../images/blogskeep-hero.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    transform: matrix(1, 0, 0, 1, 0, 0);
+  }
+`
+export const ProjectSection = styled.div`
+  width: 100%;
+
+  .project-section {
+    align-items: center;
+
+    h2 {
+      ${HeadingSecondary};
+      text-transform: uppercase;
+      text-align: center;
+    }
+
+    p {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      vertical-align: middle;
+      max-width: 800px;
+      margin: 0 auto;
+      line-height:22px;
+      text-align: center;
+    }
+
+    .section-margin {
+      width: 90%;
+      margin: 0 auto;
+    }
+
+    .section-b {
+      /* width:100%; */
+
+      img {
+        width: 100%;
+        max-width: 1440px;
+        height: auto;
+        z-index: 0;
+      }
+    }
+  }
+`
+
+export const SectionC = styled.section`
+  position: relative;
+  width: 100%;
+  max-width: 1440px;
+  display: block;
+  border: 3px solid gold;
+
+  @media only screen and (min-width: 767px) {
+
+  }
+
+  h3 {
+    margin: 16px 0;
+  }
+
+  .browser {
+    margin: 0;
+    width: 66.666%;
+    float: right;
+    top: 120px;
+
+    @media only screen and (max-width: 800px) {
+      width: 100%
+    }
+    
+    img {
+      float: right;
+      justify-self: flex-end;
+    }
+  }
+
+  .intro-b {
+    position: absolute;
+    top: 120px;
+    width: 30%;
+    border: 2px solid green;
+    
+    @media only screen and (max-width: 800px) {
+      width: 100%;
+      float: left;
+      position: static;
+      padding-bottom: 31px;
+    }
+  }
+
+  .project-description {
+    text-align: left;
+    line-height: 22px;
+    color: #313131;
+  }
+
+  .section-padding {
+    padding: 51px 0;
+    padding-bottom: 0;
   }
 `
